72dde55f57229ae1af0daecaa3f591dbd166a425
t73 <142630595+tcarver9@users.noreply.github.com>
2025-07-09 12:13:53 -0400

    Update disable-inactive-accounts.ps1

--- disable-inactive-accounts.ps1
+++ disable-inactive-accounts.ps1
@@ -1,1 +1,10 @@
+# Script to disable accounts that have not logged in for 90+ days
 
+$Threshold = (Get-Date).AddDays(-90)
+
+Search-ADAccount -AccountInactive -UsersOnly -TimeSpan 90.00:00:00 |
+Where-Object { $_.LastLogonDate -lt $Threshold -and $_.Enabled -eq $true } |
+ForEach-Object {
+    Disable-ADAccount -Identity $_.SamAccountName
+    Write-Host "Disabled account:" $_.SamAccountName
+}
