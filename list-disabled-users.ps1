7865d817f03dbf2acad956e5c2cd2242407cef08
t73 <142630595+tcarver9@users.noreply.github.com>
2025-07-09 12:13:25 -0400

    Update list-disabled-users.ps1

--- list-disabled-users.ps1
+++ list-disabled-users.ps1
@@ -1,1 +1,7 @@
+# Script to list all disabled user accounts in Active Directory
 
+Get-ADUser -Filter 'Enabled -eq $false' -Properties Name, Enabled |
+Select-Object Name, Enabled |
+Out-GridView -Title "Disabled AD Users"
+
+# | Export-Csv -Path "disabled_users.csv" -NoTypeInformation
