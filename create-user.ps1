edf56fa72025028885858e6d21abb43b8cf1aa33
t73 <142630595+tcarver9@users.noreply.github.com>
2025-07-09 12:10:51 -0400

    Update create-user.ps1

--- create-user.ps1
+++ create-user.ps1
@@ -1,1 +1,22 @@
+# Script to create a new user in Active Directory
 
+# Set user details
+$FirstName = "Tyler"
+$LastName = "Carver"
+$Username = "tcarver"
+$DisplayName = "$FirstName $LastName"
+$Password = ConvertTo-SecureString "P@ssword123" -AsPlainText -Force
+$OU = "OU=Users,DC=yourdomain,DC=com" # Change this to match your environment
+
+# Create the user
+New-ADUser -Name $DisplayName `
+  -GivenName $FirstName `
+  -Surname $LastName `
+  -SamAccountName $Username `
+  -UserPrincipalName "$Username@yourdomain.com" `
+  -AccountPassword $Password `
+  -DisplayName $DisplayName `
+  -Enabled $true `
+  -Path $OU
+
+Write-Host "User $DisplayName created successfully."
