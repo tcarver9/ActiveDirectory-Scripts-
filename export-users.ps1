467a7f577381ec3e3664342eb3f65ae4427353e9
t73 <142630595+tcarver9@users.noreply.github.com>
2025-07-09 12:12:51 -0400

    Update export-users.ps1

--- export-users.ps1
+++ export-users.ps1
@@ -1,1 +1,7 @@
+# Script to export all users with key properties to a CSV file
 
+Get-ADUser -Filter * -Property Name, EmailAddress, Department |
+Select-Object Name, EmailAddress, Department |
+Export-Csv -Path "C:\ADUsers.csv" -NoTypeInformation
+
+Write-Host "Exported all users to C:\ADUsers.csv"
