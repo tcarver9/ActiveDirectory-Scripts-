f7e7c0d1914602b7ccfab907d15665aa6da95b66
t73 <142630595+tcarver9@users.noreply.github.com>
2025-07-09 12:11:24 -0400

    Update reset-password.ps1

--- reset-password.ps1
+++ reset-password.ps1
@@ -1,1 +1,9 @@
+# Script to reset a user's password and unlock the account
 
+$Username = "tcarver"
+$NewPassword = ConvertTo-SecureString "N3wP@ssword!" -AsPlainText -Force
+
+Set-ADAccountPassword -Identity $Username -Reset -NewPassword $NewPassword
+Unlock-ADAccount -Identity $Username
+
+Write-Host "Password reset and account unlocked for $Username."
